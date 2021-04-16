f7d39c699c93f817ec965462f2749d235579d43c
Oleksii Novoseltsev <novosaleks@gmail.com>
2021-04-16 21:09:24 +0300

    navbar + selection menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import React from "react";
-import RoutingContainer from "./containers/routing-container";
+import React from 'react';
+import RoutingContainer from './containers/routing-container';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
+import NavbarContainer from './containers/navbar-container/navbar-container';
 
 const App = () => {
-    return <div>
-        <RoutingContainer/>
-    </div>;
+    return (
+        <div>
+            <NavbarContainer/>
+            <RoutingContainer/>
+        </div>
+    );
 };
 
 export default App;
